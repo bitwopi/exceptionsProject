cca9b0c1beca8162b95b4a09c6e55a105c295013
bitwopi <donmormajor@gmail.com>
2023-10-11 11:34:19 +0300

    changed loginUSer method, checkString method deleted

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,23 @@ import exceptions.WrongPasswordException;
 import java.util.regex.Pattern;
 
 public class Main {
-    // Проверяет соответствует ли строка условиям задачи, т.е. есть ли в строке символы помимо
-    // латинских и знака подчеркивания и привышает ли длина строки максимальную
-    public static boolean checkWrongString(String text){
-        return !Pattern.matches("^[a-zA-Z0-9_]{1,20}$", text);
-    }
 
     // Выкидывает исключение если логин или пароль не соответсвуют условиям
     public static void loginUser(String login, String password, String confirmPassword){
-        if (checkWrongString(login)) {
-            throw new WrongLoginException("Your login is incorrect, pls correct it!");
+        try {
+            if (!Pattern.matches("^[a-zA-Z0-9_]{1,20}$", login)) {
+                throw new WrongLoginException("Your login is incorrect, pls correct it!");
+            }
+            if (!Pattern.matches("^[a-zA-Z0-9_]{1,19}$", password) || !password.equals(confirmPassword)){
+                throw new WrongPasswordException("You've entered the wrong password or your passwords don't match!");
+            }
+        } catch (WrongLoginException | WrongPasswordException e){
+            System.out.println(e.getMessage());
         }
-        if (checkWrongString(password) || !password.equals(confirmPassword)){
-            throw new WrongPasswordException("You've entered the wrong password or your passwords don't match!");
-        }
+
 
     }
     public static void main(String[] args) {
-        try{
-            loginUser("User_", "password_", "password_");
-        }catch (WrongLoginException | WrongPasswordException e){
-            System.out.println("Your login or password is incorrect!");
-        }
+        loginUser("User_", "1111111111111111111", "1111111111111111111");
     }
 }
